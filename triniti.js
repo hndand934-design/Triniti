f6b018314d62b9919480cc09ec2d1f6a2e2ac08b
hndand934-design <hndand934@gmail.com>
2026-02-22 18:36:08 +0300

    Update triniti.js

--- triniti.js
+++ triniti.js
@@ -1,1 +1,15 @@
+(() => {
+  const tgBtn = document.getElementById("tgBtn");
+  const vkBtn = document.getElementById("vkBtn");
 
+  // пока заглушки — просто показываем alert
+  tgBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("Telegram кнопка (пока заглушка).");
+  });
+
+  vkBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("VK кнопка (пока заглушка).");
+  });
+})();
